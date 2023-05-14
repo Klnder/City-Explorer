18c115d08e735d401ee09e06b442c0f171046ac3
Jeremy Merlin <jeremymerlin@hotmail.fr>
2023-05-14 22:13:19 +0100

    movie modal css

--- src/Components/Main/Movie/MovieModal.jsx
+++ src/Components/Main/Movie/MovieModal.jsx
@@ -1,19 +1,45 @@
 import React from "react";
 import "./MovieModal.css";
 
-export default function MovieModal(props) {
+export default function MovieModal({ movie, handleModal, imgUrl }) {
   return (
-    <div className="movie-modal" onClick={props.handleModal}>
+    <div className="movie-modal" onClick={handleModal}>
       <div className="grid">
         <div className="Img">
-          <img className="modal-img-movie" src={props.imgUrl} alt="movie" />
+          <img className="modal-img-movie" src={imgUrl} alt="movie" />
         </div>
-        <div className="Details">
-          <p>Title: {props.movie.title}</p>
-          <p>Overview: {props.movie.overview}</p>
-          <p>Average Votes: {props.movie.average_votes}</p>
-          <p>Popularity: {props.movie.popularity}</p>
-          <p>Released date: {props.movie.released_on}</p>
+
+        <div className="details">
+          <div className="title">
+            <p>Title:</p>
+          </div>
+          <div className="overview">
+            <p>Overview:</p>
+          </div>
+          <div className="average-vote">
+            <p>Average Votes:</p>
+          </div>
+          <div className="popularity">
+            <p>Popularity:</p>
+          </div>
+          <div className="released-date">
+            <p>Released date:</p>
+          </div>
+          <div className="title-data">
+            <p>{movie.title}</p>
+          </div>
+          <div className="overview-data">
+            <p>{movie.overview}</p>
+          </div>
+          <div className="average-vote-data">
+            <p>{movie.average_votes}</p>
+          </div>
+          <div className="popularity-data">
+            <p>{movie.popularity}</p>
+          </div>
+          <div className="released-date-data">
+            <p>{movie.released_on}</p>
+          </div>
         </div>
       </div>
     </div>
